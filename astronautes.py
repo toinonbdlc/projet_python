d4c126b38f83ed4bfa9d378adf10b28fb0eaf246
toinonbdlc <toinon.bdlc@laposte.net>
2022-03-11 11:07:41 +0100

    Revert "Astronautes craft = ISS"
    This reverts commit 800aa30f1625414d2c23c1433c8fb35804279e43.

--- astronautes.py
+++ astronautes.py
@@ -2,5 +2,4 @@ import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
-    if personne['craft'] == "ISS": 
-        print(personne)
+    print(personne)
